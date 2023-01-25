34fc2866ce9e2aec3f07846ca69becfe82965567
João Antônio Gomes <joao.antonio@softplan.com.br>
2023-01-25 08:28:51 -0300

    Alterado auto complete para listar caso nome seja nulo

--- src/main/java/com/laridosos/rest/medicine/MedicineRepository.java
+++ src/main/java/com/laridosos/rest/medicine/MedicineRepository.java
@@ -3,7 +3,10 @@ package com.laridosos.rest.medicine;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MedicineRepository extends JpaRepository<Medicine, Long> {
+
+    @Query("SELECT p FROM Medicine p WHERE (:name is null or LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%')))")
     Page<Medicine> findAllByNameIgnoreCaseContaining(String name, Pageable pageable);
 }
--- src/main/java/com/laridosos/rest/patient/PatientRepository.java
+++ src/main/java/com/laridosos/rest/patient/PatientRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Range;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    @Query("SELECT p FROM Patient p WHERE (:name is null or LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%')))")
     Page<Patient> findAllByNameIgnoreCaseContaining(String name, Pageable pageable);
 }
